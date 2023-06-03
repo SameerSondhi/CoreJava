d6bc36fd767418a71e1a5bd0e69d9df4b2b2d079
Sameer Sondhi <110759750+SameerSondhi@users.noreply.github.com>
2023-06-02 22:43:55 -0700

    Add files via upload

---
+++ Coding Exercises/SecondsAndMinutes.java
@@ -0,0 +1,18 @@
+public class SecondsAndMinutes {
+
+    public static void main(String[] args){
+
+    }
+
+    public static String getDurationString(int seconds){
+
+
+    }
+
+    public static String getDurationString(int minutes, int seconds){
+        int hours = 0;
+        if(minutes>=60){
+
+        }
+    }
+}
